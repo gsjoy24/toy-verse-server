d6158690d6ca1dbc286998335b710b099cb8964e
gourjoy <goursaha307@gmail.com>
2023-05-19 19:27:35 +0600

    get toy by id method added

--- index.js
+++ index.js
@@ -47,6 +47,13 @@ async function run() {
 			res.send(result);
 		});
 
+		app.get('/toys/:id', async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+			const result = await toyCollection.findOne(query);
+			res.send(result);
+		});
+
 		app.delete('/toys/:id', async (req, res) => {
 			const id = req.params.id;
 			const query = { _id: new ObjectId(id) };

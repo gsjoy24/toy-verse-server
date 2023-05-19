181ab4eec53cdb3ea118803892889ae321b5ff44
gourjoy <goursaha307@gmail.com>
2023-05-19 21:30:18 +0600

    Patch method has been added for updated toy info

--- index.js
+++ index.js
@@ -54,6 +54,17 @@ async function run() {
 			res.send(result);
 		});
 
+		app.patch('/toys/:id', async (req, res) => {
+			const id = req.params.id;
+			const toy = req.body;
+			const query = { _id: new ObjectId(id) };
+			const updatedDoc = {
+				$set: toy
+			};
+			const result = await toyCollection.updateOne(query, updatedDoc);
+			res.send(result);
+		});
+
 		app.delete('/toys/:id', async (req, res) => {
 			const id = req.params.id;
 			const query = { _id: new ObjectId(id) };
